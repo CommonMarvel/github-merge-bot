af3a0d01175bbad709a0a24289ec6df4130b8551
carlos <echoulen@gmail.com>
2020-03-19 12:56:55 +0800

    init commit

---
+++ src/main/kotlin/com/github/merge/bot/GithubMergeBotApplication.kt
@@ -0,0 +1,11 @@
+package com.github.merge.bot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubMergeBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubMergeBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/merge/bot/GithubMergeBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.merge.bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubMergeBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
